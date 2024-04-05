c39488ded4e622c086bd919af689a64d727bbf8a
DermottB <D.Boylan@sms.ed.ac.uk>
2024-04-05 17:50:57 +0100

    Finished the header file

--- quiche/include/psp_packet_handling.h
+++ quiche/include/psp_packet_handling.h
@@ -1,8 +1,6 @@
 #ifndef PSP_PACKET_HANDLING_H
 #define PSP_PACKET_HANDLING_H
 
-#include <stdint.h>
-#include <stddef.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
@@ -16,7 +14,21 @@
 #include <openssl/rand.h>
 #include <openssl/sha.h>
 #include <openssl/cmac.h>
+#include <openssl/params.h>
+#include <openssl/core_names.h>
 #include <sqlite3.h>
 
+// Need: constants, structs, and functions for packet handling
 
+struct psphdr;
 
+uint32_t counter_one;
+uint32_t counter_two;
+uint32_t label;
+uint32_t length;
+
+unsigned char *psp_key_derivation(unsigned char *buf, uint32_t spi);
+ssize_t quiche_encrypt_psp(uint8_t *dst, uint8_t *src, size_t pkt_len, struct sockaddr_in socket, uint16_t src_port, uint64_t stream_id);
+ssize_t quiche_decrypt_psp(uint8_t *dst, uint8_t *src, size_t pkt_len, uint64_t stream);
+
+#endif
